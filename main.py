e2f147f578a7f0e5f6f8a66a1fcab6dee04b8642
zizytd <onyitoc@yahoo.com>
2024-08-18 19:06:53 +0200

    Adding retry logic and formatting the code

--- main.py
+++ main.py
@@ -4,39 +4,80 @@ import warnings
 import libsql_experimental as libsql
 from spotify import *
 
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 
-access_token = return_access_token(os.getenv('client_id_spotify'),os.getenv('client_secret_spotify'),os.getenv('refresh_token'))
+access_token = return_access_token(
+    os.getenv("client_id_spotify"),
+    os.getenv("client_secret_spotify"),
+    os.getenv("refresh_token"),
+)
 headers = {"Authorization": f"Bearer {access_token}"}
 
-tracks_items,artists_ids = last_played_50(50,headers)
+tracks_items, artists_ids = last_played_50(50, headers)
 
-artists_details = get_artists_ids_details(artists_ids,50,headers)
-
-
-conn = libsql.connect(os.getenv('turso_db'),sync_url=os.getenv('turso_db_url'), auth_token=os.getenv('turso_auth_token'))
-
-previous_listened_spotify = 'previous_listened_spotify_tracks_table'
-artists_listened = 'artists_listened_table'
-primary_key_listened = '(id, played_at)'
-primary_key_artist = '(id)'
-previous_listened_spotify_column = f'id TEXT,name TEXT,track_url TEXT,track_preview_url TEXT,artists_ids TEXT,played_at TEXT,duration_ms INTEGER,PRIMARY KEY {primary_key_listened}'
-artists_listened_column = f'id TEXT,name TEXT,genres TEXT,followers INTEGER,image_url TEXT,PRIMARY KEY {primary_key_artist}'
+artists_details = get_artists_ids_details(artists_ids, 50, headers)
 
 
-def load_data_2_db(table_name: str, table_columns: str, data: list, primary_key: str, conflict='NOTHING'):
+conn = libsql.connect(
+    os.getenv("turso_db"),
+    sync_url=os.getenv("turso_db_url"),
+    auth_token=os.getenv("turso_auth_token"),
+)
+
+previous_listened_spotify = "previous_listened_spotify_tracks_table"
+artists_listened = "artists_listened_table"
+primary_key_listened = "(id, played_at)"
+primary_key_artist = "(id)"
+previous_listened_spotify_column = f"id TEXT,name TEXT,track_url TEXT,track_preview_url TEXT,artists_ids TEXT,played_at TEXT,duration_ms INTEGER,PRIMARY KEY {primary_key_listened}"
+artists_listened_column = f"id TEXT,name TEXT,genres TEXT,followers INTEGER,image_url TEXT,PRIMARY KEY {primary_key_artist}"
+
+
+def load_data_2_db(
+    table_name: str,
+    table_columns: str,
+    data: list,
+    primary_key: str,
+    conflict="NOTHING",
+):
     conn.execute(f"CREATE TABLE IF NOT EXISTS {table_name}({table_columns})")
     exe_many = f"INSERT INTO {table_name} VALUES ({', '.join('?'*len(data[0]))}) ON CONFLICT {primary_key} DO {conflict}"
-    if conflict == 'UPDATE SET':
-        combined_str = ', '.join([f'{i.split()[0]} = excluded.{i.split()[0]}' for i in table_columns.split(',')][1:-2])
+    if conflict == "UPDATE SET":
+        combined_str = ", ".join(
+            [
+                f"{i.split()[0]} = excluded.{i.split()[0]}"
+                for i in table_columns.split(",")
+            ][1:-2]
+        )
         exe_many = f"INSERT INTO {table_name} VALUES ({', '.join('?'*len(data[0]))}) ON CONFLICT{primary_key} DO {conflict} {combined_str}"
     chunksize = 20
-    for i in range(0,len(data),chunksize):
-        conn.executemany(exe_many,
-            data[i:i+chunksize],
-            )
-        conn.commit()
+    attempt = 3
+    for h in range(attempt):
+        try:
+            for i in range(0, len(data), chunksize):
+                conn.executemany(
+                    exe_many,
+                    data[i : i + chunksize],
+                )
+                conn.commit()
+                break
+        except ValueError:
+            if h < attempt - 1:
+                print(f"Transaction timed out. Retrying.")
+            else:
+                raise
 
-load_data_2_db(previous_listened_spotify,previous_listened_spotify_column,tracks_items,primary_key_listened)
+
+load_data_2_db(
+    previous_listened_spotify,
+    previous_listened_spotify_column,
+    tracks_items,
+    primary_key_listened,
+)
 sleep(2)
-load_data_2_db(artists_listened,artists_listened_column,artists_details,primary_key_artist,'UPDATE SET')
+load_data_2_db(
+    artists_listened,
+    artists_listened_column,
+    artists_details,
+    primary_key_artist,
+    "UPDATE SET",
+)
--- spotify.py
+++ spotify.py
@@ -4,42 +4,76 @@ import requests
 from requests.adapters import HTTPAdapter
 from time import sleep
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 session = requests.Session()
-session.mount('http://', HTTPAdapter(max_retries=5))
-session.mount('https://', HTTPAdapter(max_retries=5))
+session.mount("http://", HTTPAdapter(max_retries=5))
+session.mount("https://", HTTPAdapter(max_retries=5))
+
 
 def return_access_token(client_id: str, client_secret: str, refresh_token: str):
     headers_token = {
-    "Authorization": requests.auth._basic_auth_str(client_id,client_secret),
-    "Content-Type": "application/x-www-form-urlencoded"
+        "Authorization": requests.auth._basic_auth_str(client_id, client_secret),
+        "Content-Type": "application/x-www-form-urlencoded",
     }
     token_url = "https://accounts.spotify.com/api/token"
-    data = {
-        "grant_type": "refresh_token",
-        "refresh_token": refresh_token
-    }
+    data = {"grant_type": "refresh_token", "refresh_token": refresh_token}
     response = session.post(token_url, data=data, headers=headers_token)
-    return response.json()['access_token']
+    return response.json()["access_token"]
+
 
 def last_played_50(limit: int, headers: dict):
-    params = {'limit':limit}
-    last_played = session.get('https://api.spotify.com/v1/me/player/recently-played',headers=headers,params=params)
-    tracks = last_played.json()['items']
-    tracks_items = [[track['track']['id'],track['track']['name'],track['track']['external_urls']['spotify'],track['track']['preview_url'],
-                     str([artist.get('id') for artist in track['track']['artists']]),track['played_at'],track['track']['duration_ms'],
-                     [artist.get('id') for artist in track['track']['artists']]] for track in tracks]
-    artists_ids = list(set([artist_id for track_item in tracks_items for artist_id in track_item[-1]]))
-    return [[tuple(track_item[:-1]) for track_item in tracks_items],artists_ids]
+    params = {"limit": limit}
+    last_played = session.get(
+        "https://api.spotify.com/v1/me/player/recently-played",
+        headers=headers,
+        params=params,
+    )
+    tracks = last_played.json()["items"]
+    tracks_items = [
+        [
+            track["track"]["id"],
+            track["track"]["name"],
+            track["track"]["external_urls"]["spotify"],
+            track["track"]["preview_url"],
+            str([artist.get("id") for artist in track["track"]["artists"]]),
+            track["played_at"],
+            track["track"]["duration_ms"],
+            [artist.get("id") for artist in track["track"]["artists"]],
+        ]
+        for track in tracks
+    ]
+    artists_ids = list(
+        set([artist_id for track_item in tracks_items for artist_id in track_item[-1]])
+    )
+    return [[tuple(track_item[:-1]) for track_item in tracks_items], artists_ids]
+
 
 def get_artists_ids_details(artists_ids: list, chunksize: int, headers: dict):
     artists_items = []
-    for i in range(0,len(artists_ids),chunksize):
-      artists_ids_chunk = ','.join(artists_ids[i:i+chunksize])
-      params_ids = {'ids':artists_ids_chunk}
-      artists = session.get('https://api.spotify.com/v1/artists',headers=headers,params=params_ids)
-      artists_res = artists.json()
-      artists_items.append([[i['id'],i['name'],json.dumps((i['genres'])),i['followers']['total'],i['images'][-1]['url']] for i in artists_res['artists']])
-      sleep(2)
-    return [tuple(artist_item) for artists_item in artists_items for artist_item in artists_item]
+    for i in range(0, len(artists_ids), chunksize):
+        artists_ids_chunk = ",".join(artists_ids[i : i + chunksize])
+        params_ids = {"ids": artists_ids_chunk}
+        artists = session.get(
+            "https://api.spotify.com/v1/artists", headers=headers, params=params_ids
+        )
+        artists_res = artists.json()
+        artists_items.append(
+            [
+                [
+                    i["id"],
+                    i["name"],
+                    json.dumps((i["genres"])),
+                    i["followers"]["total"],
+                    i["images"][-1]["url"],
+                ]
+                for i in artists_res["artists"]
+            ]
+        )
+        sleep(2)
+    return [
+        tuple(artist_item)
+        for artists_item in artists_items
+        for artist_item in artists_item
+    ]
